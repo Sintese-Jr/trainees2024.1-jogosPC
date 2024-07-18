647f102117421878e85133a33ea79ba7623ba249
melogtm <gabrielcursos@proton.me>
2024-07-17 21:09:44 -0300

    feat: game image

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -5,12 +5,36 @@ async function connectDatabase() {
     mongoose.connect("mongodb+srv://back:WZsfQutJJgXOGzxw@cluster0.ih9ygex.mongodb.net/Jogos");
 }
 
-async function ListarJogos() {
-    return await game.find();
+async function ListarJogos(limite = null) {
+    if (limite == null) {
+        return await game.find();
+    } 
+    return await game.find().limit(limite);
 }
 
 async function BuscarJogos(nome) {
     return await game.find({Game: new RegExp(nome, 'i')});
 }
 
-export {connectDatabase, ListarJogos, BuscarJogos};
+async function BuscarImagemJogo(nome) {
+    return fetch(`https://api.rawg.io/api/games?key=ac9af96231f64ff09afe969f2e97c770&search=${nome}`)
+    .then(response => response.json())
+    .then(data => {
+        // Se não encontramos o jogo, retornamos uma string vazia
+        if (data.count != 0) return data.results[0].background_image;
+        
+        /*
+        Repare que um jogo nessa API pode não estar na Coleção de Jogos que temos no banco de dados.
+        
+        Isso será tratado no servidor.
+        */
+
+        return ""; 
+    })
+    .catch(error => {
+        console.log("Erro ao buscar imagem do jogo: " + error.message);
+        return ""; 
+    });
+}
+
+export {connectDatabase, ListarJogos, BuscarJogos, BuscarImagemJogo};
