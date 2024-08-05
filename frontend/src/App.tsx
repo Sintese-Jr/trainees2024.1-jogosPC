5052fd545773ca7bfba6a1266d2f2dbb6174387c
jvpascale <jvdepascalesouza@gmail.com>
2024-08-04 23:03:00 -0300

    Add: Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import Catalogo from "./components/Catalogo/index.tsx";
 import React from "react";
+import Footer from "./components/Footer/Footer.tsx";
 
 function App() {
 
   return (
     <>
       <Catalogo />
+
+
+      
+      <Footer/>
     </>
   );
 }
